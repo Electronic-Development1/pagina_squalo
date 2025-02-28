5e0a29ebc28db0f8690104f50235955750673028
Electronic-Development1 <equipodetelemetria@gmail.com>
2025-02-28 09:25:29 -0500

    Delete pagina_squalo directory

--- pagina_squalo/app_pagina_squalo/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from app_pagina_squalo.models import Usuarios
-
-# Register your models here.
-
-admin.site.register(Usuarios)
--- pagina_squalo/app_pagina_squalo/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppPaginaSqualoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_pagina_squalo'
